a2327953be45ec8fd8d3c5214f33c3832a1f4c65
lequocvieet <lequocviethk19@gmail.com>
2022-12-21 14:42:31 +0700

    test github hook

--- golang_service/mock_time/mock.go
+++ golang_service/mock_time/mock.go
@@ -3,6 +3,6 @@ package mock_time
 import "time"
 
 type Clock interface {
-	Now() time.Time
-	After(d time.Duration) <-chan time.Time
+	Now(now time.Time) time.Time
+	//After(d time.Duration) <-chan time.Time
 }
